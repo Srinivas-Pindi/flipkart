e364bc0a6152069ec3d1e871043d92b6f8c711c4
Srinivas-Pindi <pindisrinivas1@gmail.com>
2023-10-12 12:05:05 +0530

    init commit

---
+++ myapp5-master/src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+//import arc from './arc.jpg';
+import Sidebar from './components/Sidebar';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <>
+   
+    <Products/>
+    {/* <img src={arc} alt="mo preview"/> */}
+    </>
+  );
+}
+
+export default App;
